35a667d84239288641a25a9817d76c9ecf95e793
Reper <43357489+Repertix@users.noreply.github.com>
2021-07-08 15:33:05 -0500

    Update Debug.gd

--- Debug.gd
+++ Debug.gd
@@ -5,4 +5,8 @@ func _ready():
 	$Control/VBoxContainer/HBoxContainer3/Label.set_text("Ants: "+str(ants))
 
 func updateAnt():
+	if ants == 0:
+		print("All ants said goodbye! Hiring new ants...")
+		get_tree().reload_current_scene()
+		return
 	$Control/VBoxContainer/HBoxContainer3/Label.set_text("Ants: "+str(ants))
